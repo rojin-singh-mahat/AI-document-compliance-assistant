94ab554feb7a0684215db06dd34985205d6e450a
Rojin Singh Mahat <np03cs4a230528@heraldcollege.edu.np>
2026-09-02 00:46:23 +0545

    added document upload endpoint; pdf to txt next

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
+import os
+from pypdf import PdfReader
 
 app = FastAPI(title="AI Document & Compliance Assistant")
 
@@ -15,3 +17,16 @@ async def root():
     return {
         "message": "AI Document & Compliance Assistant API is running"
     }
+
+@app.post("/documents")
+async def upload_document(file: UploadFile = File(...)):
+    if file.content_type not in ["application/pdf", "application/vnd.openxmlformats-officedocument.wordprocessingml.document", "text/plain"]:
+        return {"error":"Content type not supported"}
+    
+    os.makedirs("uploads", exist_ok = True)
+
+    with open(f"uploads/{file.filename}", "wb") as buffer:
+        buffer.write(await file.read())
+
+def extract_text_from_pdf(file_path):
+    pdf = PDFReader
